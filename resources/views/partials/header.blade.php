d3069c588e6a8510a88f22d66e3e60c756ed09a0
Aliyu Abubakar <aliyufari@gmail.com>
2026-03-12 14:16:13 +0100

    Fixed CVR scope

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -139,8 +139,9 @@
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
             <img src="{{auth()->user()->image ? asset('storage/' . auth()->user()->image) : asset('assets/img/users/user.jpg')}}" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">{{ucfirst(auth()->user()->role->name)}}</span>
-          </a><!-- End Profile Iamge Icon -->
+            <span class="d-none d-md-block dropdown-toggle ps-2">
+              {{ ucfirst(auth()->user()->role?->name ?? '') }}
+            </span>
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             
